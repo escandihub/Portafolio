024ac96f3c0c89b4158daf1701db9d37345262e3
Escandon <aleskndon@hotmail.com>
2020-11-18 21:36:41 -0600

    limpiando el codigo

---
+++ src/utils/experiencias.js
@@ -0,0 +1,95 @@
+export function exp(){
+  let array = [
+    {
+      id: 9,
+      proyecto: "Sistema SARE",
+      desc:
+        "Sistema para la administración de de capturas de empresas y las licencias que se emiten con 80% para finalizar, es para una Institucion publica de Tuxtla Gutierrez, Chiapas, Mexico.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/SARE-frontend' target='_blank'> Repositorio </a>",
+      tecn: ["Vue 2.X", "Laravel 8", "Vuex", "vue-route"],
+      date: "2020 - Nov - 3 semanas",
+    },
+    {
+      id: 8,
+      proyecto: "App-Ter",
+      desc:
+        "Aplicacion en desarrollo [pausado] que sera universal Android, IOS, que administrara la produccion de ganaderos en entornos urbanos para conocer en cualquier momento lo que ha producido.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/app-ter.git' target='_blank'> Repositorio </a>",
+      tecn: ["Angular 10", "Ionic 5", "NgRx", "Capacitor"],
+      date: "2020 - Sep - 2 semanas",
+    },
+    {
+      id: 7,
+      proyecto: "Basic Push-Notification",
+      desc:
+        "Para conocer de cerca la interacción de los Services Worker, contruí una pequeña aplicacion para conocer su funcionamiento.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/basic-push-notification' target='_blank'> Repositorio </a>",
+      tecn: ["Node.js", "Express"],
+      date: "2020 - Feb",
+    },
+    {
+      id: 4,
+      proyecto: "Reingenieria al Sistema para la Formacion y Actualización Profesional Docente",
+      desc:
+        "En mi residencia profesional desarrolle un Sistema web moderno, que aguiliza el proceso que se llevaba se acabo de una manera tradicional, facilitando la busqueda de la información y la organizacion.",
+      url:
+        " <a class='link-on-bg' href='https://fodap.ittg.edu.mx/' target='_blank'> FODAP </a>",
+      tecn: ["Laravel", "Vue.JS", "Mysql", "Vuetify"],
+      date: "2020 - Ene - 6M",
+    },
+    {
+      id: 5,
+      proyecto: "Deteccion de Sospechosos",
+      desc:
+        "En la participación del Hackaton organizado por la UPCH, mi equipo contruimos una aplicacion para detectar con las camaras un presunto sospechoso para poder dar una alerta a las autoridades.",
+      url:
+        " <a class='link-on-bg' href='#' target='_blank'> Repositorio </a>",
+      tecn: ["Laravel 5.8"],
+      date: "2019 - Dec",
+    },
+    {
+      id: 6,
+      proyecto: "Expreciones Regulares",
+      desc:
+        "He desarrolado una mini-aplicacion en Angular, para detectar cuando una expresion que ingrese el usuario ha sido correcta.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/Expeciones-Regulares' target='_blank'> Repositorio </a>",
+      tecn: ["Angular", "TypeScrip"],
+      date: "2018 - Marzo",
+    },
+    {
+      id: 3,
+      proyecto: "Pagina de Aseguradora",
+      desc:
+        "Se desarrollo una aplicacion donde se presenta informacion acerca de los seguros.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/Aseguradora' target='_blank'> Repositorio FronEnd </a> </br>  <a class='link-on-bg' href='https://github.com/escandihub/deployHDI' target='_blank'> Repositorio Backend </a>",
+      tecn: ["Angular 5", "Node.js"],
+      date: "2018 - Jun",
+    },
+    {
+      id: 2,
+      proyecto: "CLI Python",
+      desc:
+        "En la materias de LENG.Y AUTOM. se desarrollo una aplicacion para facilitar el desarrollo con el micro-Framework Flask, pudiendo crear el proyecto base en el patron MVC.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/DarkUnknowKnigth/thunder' target='_blank'> Repositorio </a>",
+      tecn: ["Pyhon", "Html"],
+      date: "2018 - Dec",
+    },
+    {
+      id: 1,
+      proyecto: "Laravel API Auth",
+      desc:
+        "Con la implementacion de Laravel 5.8, y la libreria de Passport creé una API para autenticar los usuarios y a su vez enviar correos a los usuarios.",
+      url:
+        " <a class='link-on-bg' href='https://github.com/escandihub/SPA' target='_blank'> Repositorio </a>",
+      tecn: ["Laravel 5.8", "Passport"],
+      date: "2018 - Dec",
+    },
+  ];
+  return  array;
+}
